c9703d6f3d4816afcff91fea59d9d2f7eadbc2f9
Ethzal <daniel.flores.jimenez.3a@gmail.com>
2026-03-20 10:13:46 +0100

    refactor(core): create DateUtils.kt in core module to format dates
    - Add core dependency to presentation module.

---
+++ core/src/main/java/com/iberdrola/practicas2026/core/utils/DateUtils.kt
@@ -0,0 +1,15 @@
+package com.iberdrola.practicas2026.core.utils
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+fun String.toUiDate(): String {
+    return try {
+        val parsed = LocalDate.parse(this)
+        val formatter = DateTimeFormatter.ofPattern("d 'de' MMMM", Locale.forLanguageTag("es-ES"))
+        parsed.format(formatter)
+    } catch (_: Exception) {
+        "FALLO: $this" // Si falla, devuelve el original
+    }
+}
