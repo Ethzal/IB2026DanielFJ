136862e9ff9264bddd5d7138c0a1f5d37d2948ab
Daniel Flores <160422773+Ethzal@users.noreply.github.com>
2026-04-21 14:50:10 +0200

    feat(preview): add previews for InvoiceRow and LastInvoiceCard

---
+++ presentation/src/main/java/com/iberdrola/practicas2026/presentation/ui/preview/InvoiceRowPreview.kt
@@ -0,0 +1,29 @@
+package com.iberdrola.practicas2026.presentation.ui.preview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.iberdrola.practicas2026.domain.model.Invoice
+import com.iberdrola.practicas2026.presentation.composables.common.InvoiceRow
+import com.iberdrola.practicas2026.presentation.ui.theme.EnergyAppTheme
+
+@Preview(showBackground = true)
+@Composable
+fun InvoiceRowPreview() {
+
+    EnergyAppTheme {
+        val fakeInvoice = Invoice(
+            date = "2024-03-08",
+            type = "Factura Luz",
+            amount = 20.00,
+            status = "Pendiente de Pago",
+            id = "2",
+            startDate = "2024-02-08",
+            endDate = "2024-03-08"
+        )
+
+        InvoiceRow(
+            invoice = fakeInvoice,
+            onClick = {}
+        )
+    }
+}
---
+++ presentation/src/main/java/com/iberdrola/practicas2026/presentation/ui/preview/LastInvoiceCardPreview.kt
@@ -0,0 +1,29 @@
+package com.iberdrola.practicas2026.presentation.ui.preview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.iberdrola.practicas2026.domain.model.Invoice
+import com.iberdrola.practicas2026.presentation.composables.invoice.LastInvoiceCard
+import com.iberdrola.practicas2026.presentation.ui.theme.EnergyAppTheme
+
+@Preview(showBackground = true)
+@Composable
+fun LastInvoiceCardPreview() {
+
+    EnergyAppTheme{
+        val fakeInvoice = Invoice(
+            type = "Factura Luz",
+            amount = 20.00,
+            startDate = "2024-02-01",
+            endDate = "2024-03-04",
+            status = "Pendiente de Pago",
+            id = "3",
+            date = "2024-03-04"
+        )
+
+        LastInvoiceCard(
+            invoice = fakeInvoice,
+            onClick = {}
+        )
+    }
+}
