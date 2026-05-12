d48d0a7e491f64a1b53de8593cb9907212ef3ed3
Daniel Flores <160422773+Ethzal@users.noreply.github.com>
2026-05-12 13:10:20 +0200

    refactor(email-utils): update email validation constraints and masking logic

--- core/src/main/java/com/iberdrola/practicas2026/core/utils/EmailUtils.kt
+++ core/src/main/java/com/iberdrola/practicas2026/core/utils/EmailUtils.kt
@@ -1,14 +1,14 @@
 package com.iberdrola.practicas2026.core.utils
 
 fun isEmailValid(email: String): Boolean {
-    val emailRegex = "^[A-Za-z0-9._%+-]{5,}@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$".toRegex()
+    val emailRegex = "^[A-Za-z0-9._%+-]{1,}@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$".toRegex()
     return email.matches(emailRegex)
 }
 
 fun getEmailError(email: String): String? {
     if (email.isEmpty()) return null
 
-    val emailRegex = "^[A-Za-z0-9._%+-]{5,}@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$".toRegex()
+    val emailRegex = "^[A-Za-z0-9._%+-]{1,}@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$".toRegex()
 
     if (!email.matches(emailRegex)) {
         val parts = email.split("@")
@@ -16,8 +16,8 @@ fun getEmailError(email: String): String? {
         val domainPart = parts.getOrNull(1) ?: ""
 
         return when {
+            userPart.isEmpty() -> "Mínimo 1 carácter antes del @"
             !email.contains("@") -> "Falta el símbolo @"
-            userPart.length < 5 -> "Mínimo 5 caracteres antes del @"
             domainPart.isEmpty() -> "Introduce un dominio (ej: gmail.com)"
             !domainPart.contains(".") -> "El dominio debe incluir un punto"
             domainPart.substringAfterLast(".").length < 2 -> "El dominio debe terminar en .com, .es, etc."
@@ -33,8 +33,10 @@ fun String.maskEmail(): String {
     val user = parts[0]
     val domain = parts[1]
 
-    return if (user.length > 1) {
+    return if (user.length > 2) {
         "${user.first()}${"*".repeat(user.length - 2)}${user.last()}@$domain"
+    } else if (user.length == 2) {
+        "${user.first()}*@$domain"
     } else {
         "*@$domain"
     }
